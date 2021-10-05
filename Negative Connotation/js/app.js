7144e38565dafeb54c797c5a2a7d5c4bb12cca41
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-05 17:33:17 +0300

    connonation v2
     edited to totally solved

--- Negative Connotation/js/app.js
+++ Negative Connotation/js/app.js
@@ -1,7 +1,7 @@
 function connotation(str) {
     let pos = 0;
     let neg = 0;
-    str = str.split(/\s+/);
+    str = str.match(/\w+/gi);
     for (let i = 0; i < str.length; i++) {
         if (str[i][0].match(/[a-m]/i)) {
             pos++;
